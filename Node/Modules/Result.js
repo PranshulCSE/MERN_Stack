6687425dfb453774b5816502d2cea874656fff49
Pranshul <threjapranshul@gmail.com>
2026-01-28 09:25:11 +0530

    Understanding the concept of Modules in NODE.JS

---
+++ Node/Modules/Result.js
@@ -0,0 +1,9 @@
+// import(`./Math`); This way is for ES6 
+// Importing FUnction from MAth.JS
+
+const maths= require(`./Math`);
+console.log(maths);
+console.log(maths.add(5,6));
+console.log(maths.sub(15,6));
+console.log(maths.mul(15,6));
+console.log(maths.div(90,6));
