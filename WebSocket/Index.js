3355b83d787006326f5cc2e37de61c72863a60c7
Pranshul <threjapranshul@gmail.com>
2026-06-16 10:00:49 +0530

    Understanding io.emit

--- WebSocket/Index.js
+++ WebSocket/Index.js
@@ -1,3 +1,23 @@
 // Code to make a Websocket Connection
 const express= require ("express");
 const app=express();
+const {server} = require ("socket.io");
+const http= require("http");
+
+
+const server= http.createServer(app);
+const io = new Server(server);
+
+io.on("connection",(socket)=>{
+
+    socket.on('message',(data)=>{
+        io.emit('new-message', data);
+    })
+    socket.on("disconnect",()=>{
+        console.log("Disconnected from Server");
+    })
+})
+
+server.listen(3000,()=>{
+    console.log("Server is listening at 3000");
+})
